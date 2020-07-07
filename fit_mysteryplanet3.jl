c36f9bb3891368dac96e70e375cadf5d52954ce9
Eric Agol <agol@uw.edu>
2020-07-07 12:03:22 -0700

    Changed length of time1 and time2 vectors; add .-

--- fit_mysteryplanet3.jl
+++ fit_mysteryplanet3.jl
@@ -63,8 +63,8 @@ function fit_mysteryplanet3()
         #sets up data structure to hold planet properties, passed to TTVFaster
     jmax = 5
     data=param
-    n1 = 38
-    n2 = 24
+    n1 = length(tt1)
+    n2 = length(tt2)
     p1=TTVFaster.Planet_plane_hk(data[1],data[2],data[3],data[4],data[ 5])
     p2=TTVFaster.Planet_plane_hk(data[6],data[7],data[8],data[9],data[10])
     time1 = collect(p1.trans0 .+ range(0,stop=n1-1,length=n1) .* p1.period)
@@ -74,10 +74,9 @@ function fit_mysteryplanet3()
     ttv2 = zeros(n2)
 
     dummy=TTVFaster.compute_ttv!(jmax,p1,p2,time1,time2,ttv1,ttv2) #first call to TTVFaster w/o optim
-
-    scatter(time1,tt1-t1)
+    scatter(time1,tt1 .-t1)
     plot(time1,ttv1)
-    scatter(time2,tt2-t2,color="green")
+    scatter(time2,tt2 .-t2,color="green")
     plot(time2,ttv2)
 
         #globals are used by TTVFaster, actual observed data held fixed in optim
@@ -157,10 +156,10 @@ function fit_mysteryplanet3()
     println("Minimum: ",chi_best," Param: ",fit.param)
     println(fit.param)
 
-    scatter(time1,tt1-t1)
-    plot(time1,ttmodel[1:n1]-t1)
-    scatter(time2,tt2-t2,color="green")
-    plot(time2,ttmodel[n1+1:n1+n2]-t2)
+    scatter(time1,tt1 .-t1)
+    plot(time1,ttmodel[1:n1] .-t1)
+    scatter(time2,tt2 .-t2,color="green")
+    plot(time2,ttmodel[n1+1:n1+n2] .-t2)
 
     println("Hit return to continue")
     read(STDIN,Char)
