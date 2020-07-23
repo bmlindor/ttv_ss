ebff57bf7b914abd60f5e229f3cff1ebcb56943e
[bmlindor] <[blindor@uw.edu]>
2020-07-23 11:08:56 -0700

    update fit_p3

--- run_fitp3.jl
+++ run_fitp3.jl
@@ -4,7 +4,7 @@ include("sim_times.jl")
 include("fit_mysteryplanet3.jl")
 function run_fitp3()
 	sim = sim_times(2.4332825e6, 2.4515445e6, 1000, true, 30.0)
-	fit = fit_mysteryplanet3("noisy_ttvenus.txt","noisy_ttearth.txt",4230.0,4400.0,10,6,true)
+	fit = fit_mysteryplanet3("noisy_ttvenus.txt","noisy_ttearth.txt",4230.0,4400.0,100,6,true)
 end
 # function make_plot(filename::String)
 # 	# chi_best, pbest, chi_p3, param_p3
