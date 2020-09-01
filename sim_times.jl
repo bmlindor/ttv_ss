8ca939fca03ecebf4234ffab594e6423c35ce46f
[bmlindor] <[blindor@uw.edu]>
2020-09-01 14:15:56 -0700

    testing

--- sim_times.jl
+++ sim_times.jl
@@ -10,7 +10,8 @@ if !@isdefined(CGS)
 end
 include("regress.jl")
 
-function sim_times(jd1::Float64, jd2::Float64, Nsteps::Int64, addnoise::Bool=false, sigma::Float64=0.0, seed::Int=42)
+function sim_times(jd1::Float64, jd2::Float64, Nsteps::Int64, 
+    addnoise::Bool=false, sigma::Float64=0.0, seed::Int=42, EMB::Bool=true)
     # To do: output file with arguments in header
     #       output data frame with header?
 
@@ -42,9 +43,15 @@ function sim_times(jd1::Float64, jd2::Float64, Nsteps::Int64, addnoise::Bool=fal
     pva_earth = zeros(9, Nsteps)
     # for i=1:np0
     for i=1:Nsteps
-       pva_sun[1:9,i] = compute(eph,t0[i],0.5,10,10,options,2)
-       pva_venus[1:9,i] = compute(eph,t0[i],0.5,2,10,options,2) # useNaifId = 2 for Venus 
-       pva_earth[1:9,i] = compute(eph,t0[i],0.5,3,10,options,2) # useNaifId = 3 for Earth-Moon bary
+        pva_sun[1:9,i] = compute(eph,t0[i],0.5,10,10,options,2)
+        pva_venus[1:9,i] = compute(eph,t0[i],0.5,2,10,options,2)
+        if EMB
+            pva_earth[1:9,i] = compute(eph,t0[i],0.5,3,10,options,2) 
+        end
+        else
+            pva_earth[1:9,i] = compute(eph,t0[i],0.5,399,10,options,2)
+        end
+
     end
     L_venus = cross(pva_venus[1:3], pva_venus[4:6])
     L_earth = cross(pva_earth[1:3], pva_earth[4:6])
--- ttv_wrapper.jl
+++ ttv_wrapper.jl
@@ -1,6 +1,6 @@
 # Julia v1.1
 include("ttv_nplanet.jl")
-function ttv_wrapper(tt0, nplanet, ntrans, params, fixp3::Bool = false, p3_cur::Float64 = 0.0)
+function ttv_wrapper(tt0, nplanet, ntrans, params, fixp3::Bool = false, p3_cur::Float64 = 0.0, EMB::Bool = true)
     # These lines need modification for different choices of parameters:
     if nplanet == 2
         n1, n2 = ntrans
@@ -9,13 +9,13 @@ function ttv_wrapper(tt0, nplanet, ntrans, params, fixp3::Bool = false, p3_cur::
         n1, n2, n3 = ntrans
     end
     if nplanet == 3 && fixp3
-        param = [params[1:11];p3_cur;params[12:14]]
+        param = [params[1:11];p3_cur;params[12:end]]
     else 
         param = params
     end
     jmax = 5
     # Call ttv_nplanet:
-    ttv = ttv_nplanet(nplanet, jmax, ntrans, param)
+    ttv = ttv_nplanet(nplanet, jmax, ntrans, param[1:15])
     # We measure transit times, not TTVs, so add back in the linear ephemeris:
     t01 = params[3]
     per1 = params[2]
@@ -27,7 +27,15 @@ function ttv_wrapper(tt0, nplanet, ntrans, params, fixp3::Bool = false, p3_cur::
     per2 = params[7]
     ttv2 = collect(range(t02,stop = t02+per2*(n2-1),length = n2))
     for i=1:n2
-      ttv2[i] += ttv[2,i]
+        if EMB
+            ttv2[i] += ttv[2,i]
+        else
+            #tmax = param[end-2]; phi0 = param[end-1]; deltaphi = param[end]
+            ts = param[end-2] #tmax sinphi0
+            tc = param[end-1] #tmax cosphi0
+            deltaphi = param[end]
+            ttv2[i] += ttv[2,i] + ts*cos((i-1)*deltaphi) + tc*sin((i-1)*deltaphi)
+        end
     end
     # If transit times of additional planets were observable these would need to be added in.
     #println("param2: ",param)
