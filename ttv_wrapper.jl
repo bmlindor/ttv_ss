bee3eda1c2ca55469f9abebef50c4fd85f0f683b
[bmlindor] <[blindor@uw.edu]>
2020-09-11 12:01:41 -0700

    update fit_moon

--- ttv_wrapper.jl
+++ ttv_wrapper.jl
@@ -1,6 +1,7 @@
 # Julia v1.1
 include("ttv_nplanet.jl")
-function ttv_wrapper(tt0, nplanet, ntrans, params, EMB::Bool=false, fixp3::Bool = false, p3_cur::Float64 = 0.0)
+# function ttv_wrapper(tt0, nplanet, ntrans, params, EMB::Bool=false, fixp3::Bool = false, p3_cur::Float64 = 0.0)
+function ttv_wrapper(tt0, nplanet, ntrans, params, EMB::Bool=true)
     # These lines need modification for different choices of parameters:
     if nplanet == 2
         n1, n2 = ntrans
@@ -8,14 +9,14 @@ function ttv_wrapper(tt0, nplanet, ntrans, params, EMB::Bool=false, fixp3::Bool 
     if nplanet == 3
         n1, n2, n3 = ntrans
     end
-    if nplanet == 3 && fixp3
-        param = [params[1:11];p3_cur;params[12:end]]
-    else 
-        param = params
-    end
+    # if nplanet == 3 && fixp3
+    #     param = [params[1:11];p3_cur;params[12:end]]
+    # else 
+    #     param = params
+    # end
     jmax = 5
     # Call ttv_nplanet:
-    ttv = ttv_nplanet(nplanet, jmax, ntrans, param[1:5*nplanet])
+    ttv = ttv_nplanet(nplanet, jmax, ntrans, params[1:5*nplanet])
     # We measure transit times, not TTVs, so add back in the linear ephemeris:
     t01 = params[3]
     per1 = params[2]
@@ -31,9 +32,9 @@ function ttv_wrapper(tt0, nplanet, ntrans, params, EMB::Bool=false, fixp3::Bool 
             ttv2[i] += ttv[2,i]
         else
             #tmax = param[end-2]; phi0 = param[end-1]; deltaphi = param[end]
-            ts = param[end-2] #tmax sinphi0
-            tc = param[end-1] #tmax cosphi0
-            deltaphi = param[end]
+            ts = params[end-2] #tmax sinphi0
+            tc = params[end-1] #tmax cosphi0
+            deltaphi = params[end]
             ttv2[i] += ttv[2,i] + ts*cos((i-1)*deltaphi) + tc*sin((i-1)*deltaphi)
         end
     end
