b113089ed90a68474bd5841e4e4fcb58881273d4
[bmlindor] <[blindor@uw.edu]>
2020-09-11 13:44:26 -0700

    update fit_moon

--- chisquare.jl
+++ chisquare.jl
@@ -1,9 +1,9 @@
 # Julia v1.1
 include("ttv_wrapper.jl")
-function chisquare(tt0, nplanet, ntrans, params, tt, sigtt, fixp3::Bool = false, p3_cur::Float64 = 0.0)
+function chisquare(tt0, nplanet, ntrans, params, tt, sigtt)#, fixp3::Bool = false, p3_cur::Float64 = 0.0)
     chisq = 0.0
     # println(params, tt[1], sigtt[1])
-    tt_model = ttv_wrapper(tt0, nplanet, ntrans, params, fixp3, p3_cur)
+    tt_model = ttv_wrapper(tt0, nplanet, ntrans, params) #,fixp3, p3_cur)
     for j=1:length(tt)
       chisq += (tt[j]-tt_model[j])^2/sigtt[j]^2
     end
--- full_run.jl
+++ full_run.jl
@@ -11,14 +11,19 @@ p3in = 4230.0
 p3out = 4430.0
 np3 = 10
 nphase = 6
+dpin = 0.0 # --> 0
+dpout = 2*pi
+ndp = 6 #36
+nphiphase = 6
 sigma = 30.0 
 nsteps = 1000
 nwalkers = 50
 p3 = 10 .^ range(log10(p3in),stop=log10(p3out),length=np3)
 # function run_fitp3(label, p3in, p3out, np3, nphase, sigma)
-sim = sim_times(2.4332825e6, 2.4515445e6, 1000, true, sigma, true)
+sim = sim_times(2.4332825e6, 2.4515445e6, 1000, true, sigma, false)
 file = string("INPUTS/tt_data",sigma,"s.txt")
-fit = fit_mysteryplanet3(file,label,p3in,p3out,np3,nphase,true,sigma, true)
+# fit = fit_mysteryplanet3(file,label,p3in,p3out,np3,nphase,true,sigma, true)
+fit = fit_moon(file,label,p3in,p3out,np3,nphase,dpin, dpout, ndp, nphiphase,true,sigma)
 # @load "OUTPUTS/p3_fittestparams.jld2" #param_p3 lprob_p3 lprob_best pbest ntrans nplanet tt0 tt ttmodel sigtt p3in p3out np3 nphase
 # par_mcmc, lprob_mcmc = MCMC(pbest, label, nsteps, nwalkers, nplanet, ntrans, tt0, tt, sigtt) 
 # file = string("OUTPUTS/mcmc",label,".jld2")
