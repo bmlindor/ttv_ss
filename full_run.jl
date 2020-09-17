433101d711d2d579a08710e40ff89fcfe37432b8
[bmlindor] <[blindor@uw.edu]>
2020-09-17 15:28:43 -0700

    clarified next steps

--- full_run.jl
+++ full_run.jl
@@ -5,6 +5,7 @@ include("fit_mysteryplanet3.jl")
 include("run_mcmc.jl")
 include("fit_moon.jl")
 # sig_grid = [10.0, 15.0, 30.0, 45.0, 60.0]
+# years = [15, 30, 50]
 # p3in = 500.0; p3out = 18000.0; np3 = 1000
 # Modify the following variables as necessary:
 label = "test"
@@ -23,7 +24,7 @@ nsteps = 1000
 nwalkers = 50
 p3 = 10 .^ range(log10(p3in),stop=log10(p3out),length=np3)
 # function run_fitp3(label, p3in, p3out, np3, nphase, sigma)
-# sim = sim_times(jd1,jd2,jdsize,true,sigma,false)
+sim = sim_times(jd1,jd2,jdsize,true,sigma,false)
 # file = string("INPUTS/tt_data",sigma,"s.txt")
 # # fit = fit_mysteryplanet3(file,label,p3in,p3out,np3,nphase,true,sigma, true)
 # fit = fit_moon(file,label,p3in,p3out,np3,nphase,dpin,dpout,ndp,true,sigma)
