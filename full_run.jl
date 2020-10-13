340645b0b91bbfd9f508a6b4369ead5647e24f56
[bmlindor] <[blindor@uw.edu]>
2020-10-13 11:20:19 -0700

    try000 complete

--- full_run.jl
+++ full_run.jl
@@ -7,15 +7,15 @@ include("fit_mysteryplanet3.jl")
 # years = [15, 30, 50]
 # p3in = 500.0; p3out = 18000.0; np3 = 1000
 # Modify the following variables as necessary:
-label = "try001"
+label = "try000"
 jd1 = 2.4332825e6
 jd2 = 2.4515445e6
 jdsize = 1000
 sigma = 30.0 
-p3in = 500.0 #4230.0
-p3out = 18000.0 #4430.0
-np3 = 500 #10
-nphase = 10 #6
+p3in = 4230.0
+p3out = 4430.0
+np3 = 10
+nphase = 6
 nsteps = 10000
 nwalkers = 50
 p3 = 10 .^ range(log10(p3in),stop=log10(p3out),length=np3)
@@ -24,5 +24,5 @@ p3 = 10 .^ range(log10(p3in),stop=log10(p3out),length=np3)
 file = string("INPUTS/tt_data",sigma,"snoEMB.txt")
 @time fit = fit_mysteryplanet3(file,label,p3in,p3out,np3,nphase,true,sigma, true)
 @load "OUTP3/p3_fittestparams.jld2" #param_p3 lprob_p3 lprob_best pbest ntrans nplanet tt0 tt ttmodel sigtt p3in p3out np3 nphase
-@time par_mcmc, lprob_mcmc = MCMC(pbest, label, nsteps, nwalkers, nplanet, ntrans, tt0, tt, sigtt) #par_mcmc, lprob_mcmc, accept, iburn, steps, nwalkers, nsteps
+@time par_mcmc, lprob_mcmc = MCMC(pbest_global, label, nsteps, nwalkers, nplanet, ntrans, tt0, tt, sigtt) #par_mcmc, lprob_mcmc, accept, iburn, steps, nwalkers, nsteps
 
