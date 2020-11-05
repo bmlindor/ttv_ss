43ca2bdf9a96461527e8d6dcb2e8f545b0db5d53
Bethlee M Lindor <blindor@n2232.hyak.local>
2020-11-05 13:45:40 -0800

    made some changes for 100000 step chain

--- run_mcmc.jl
+++ run_mcmc.jl
@@ -4,17 +4,17 @@ include("MCMC.jl")
 
 # file1 = "OUTPUTS/p3_fittry001params.jld2"
 # extractdata(file1)
-# @load "OUTPUTS/p3_fittry001params.jld2" #param_p3 lprob_p3 lprob_best pbest_global ntrans nplanet tt0 tt ttmodel sigtt p3in p3out np3 nphase
-# label = "try001"
-# nsteps = 100000
-# nwalkers = 50
-# @time lprob_mcmc,par_mcmc = MCMC(pbest_global,label,nsteps,nwalkers,nplanet,ntrans,tt0,tt,sigtt,true,true) #yes EMB yes sigsys
-
-@load ("OUTPUTS/moon_fitmtestparams.jld2")
-label = "mtry1"
-nsteps = 200000
+@load "OUTPUTS/p3_fittry001params.jld2" #param_p3 lprob_p3 lprob_best pbest_global ntrans nplanet tt0 tt ttmodel sigtt p3in p3out np3 nphase
+label = "try001"
+nsteps = 100000
 nwalkers = 50
-@time lprob_mcmc,par_mcmc = MCMC(pbest_global,label,nsteps,nwalkers,nplanet,ntrans,tt0,tt,sigtt,false,true) #no EMB yes sigsys
+@time lprob_mcmc,par_mcmc = MCMC(pbest_global,label,nsteps,nwalkers,nplanet,ntrans,tt0,tt,sigtt,true,true) #yes EMB yes sigsys
+
+#@load ("OUTPUTS/moon_fitmtestparams.jld2")
+#label = "mtry1"
+#nsteps = 200000
+#nwalkers = 50
+#@time lprob_mcmc,par_mcmc = MCMC(pbest_global,label,nsteps,nwalkers,nplanet,ntrans,tt0,tt,sigtt,false,true) #no EMB yes sigsys
 
 # param = [pbest;1e-4^2]
 # nsteps = 1000
